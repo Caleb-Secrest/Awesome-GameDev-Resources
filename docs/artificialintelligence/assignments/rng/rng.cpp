2b912e8163e4058f1cd129f197301bc5862e80c2
caleb.secrest <caleb.secrest@mymail.champlain.edu>
2024-09-17 14:08:08 -0400

    Class Setup
    Doing some setup work for the rng assignment. Plan on doing different generators to produce the same result.

--- docs/artificialintelligence/assignments/rng/rng.cpp
+++ docs/artificialintelligence/assignments/rng/rng.cpp
@@ -2,15 +2,41 @@
 #include <fstream>
 #include <iostream>
 #include <istream>
+#include <random>
 const std::string TEST_FOLDER = "\\tests\\";
 unsigned int xorShift(unsigned int seed, int r1, int r2);
+unsigned int mersenneTwister(unsigned int seed, int r1, int r2);
+unsigned int middleSquareMethod(unsigned int seed, int r1, int r2);
 int main(){
   // code here
   unsigned int seed, N, min, max;
   std::cin >> seed >> N >> min >> max;
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distr(1, 3);
+
+  switch(distr(gen)) {
+    case 1:
+      xorShift(seed, min, max);
+    case 2:
+      mersenneTwister(seed, min, max);
+    case 3:
+      middleSquareMethod(seed, min, max);
+  }
 }
 //The purpose of this function is to take the number and xor shift it to output a pseudo-random number
     unsigned int xorShift(unsigned int seed, int r1, int r2)
 {
+  std::cout << 1 << std::endl;
+}
 
+unsigned int mersenneTwister(unsigned int seed, int r1, int r2)
+{
+  std::cout << 2 << std::endl;
+}
+
+unsigned int middleSquareMethod(unsigned int seed, int r1, int r2)
+{
+  std::cout << 3 << std::endl;
 }
