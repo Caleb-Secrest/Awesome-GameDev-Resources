8db912477d946d22a1f5d5615efe108042e743df
caleb.secrest <caleb.secrest@mymail.champlain.edu>
2024-09-20 22:43:10 -0400

    XorShift Implementation
    Implemented the Xorshift function. Need to get it to print multiple numbers and then I can move onto Mersenne Twister and Middle Square Method.

--- docs/artificialintelligence/assignments/rng/rng.cpp
+++ docs/artificialintelligence/assignments/rng/rng.cpp
@@ -18,25 +18,38 @@ int main(){
 
   switch(distr(gen)) {
     case 1:
-      xorShift(seed, min, max);
+      std::cout << "Xorshift: " << xorShift(seed, min, max) << std::endl;
     case 2:
       mersenneTwister(seed, min, max);
     case 3:
       middleSquareMethod(seed, min, max);
   }
+
+  std::cout << "Xorshift: " << xorShift(seed, min, max) << std::endl;
+
+  return 0;
 }
-//The purpose of this function is to take the number and xor shift it to output a pseudo-random number
-    unsigned int xorShift(unsigned int seed, int r1, int r2)
+
+
+unsigned int xorShift(unsigned int seed, int r1, int r2)
 {
-  std::cout << 1 << std::endl;
+  seed ^= (seed << 13);
+  seed ^= (seed >> 17);
+  seed ^= (seed << 5);
+
+  int min_value = std::min(r1, r2);
+  int max_value = std::max(r1, r2);
+  unsigned int result = min_value + (seed % (max_value - min_value + 1));
+
+  return result;
 }
 
 unsigned int mersenneTwister(unsigned int seed, int r1, int r2)
 {
-  std::cout << 2 << std::endl;
+  return 0;
 }
 
 unsigned int middleSquareMethod(unsigned int seed, int r1, int r2)
 {
-  std::cout << 3 << std::endl;
+  return 0;
 }
